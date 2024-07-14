6553fee80e1889609e7dca31c4f639079eaaea7d
isakasymbekuulu <isakasymbekuulu@gmail.com>
2024-07-15 00:38:50 +0600

    working test model 1.0

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ client = OpenAI(api_key='')
 
 GPT_MODEL = "gpt-4o"
 
+
 @retry(wait=wait_random_exponential(multiplier=1, max=40), stop=stop_after_attempt(3))
 def chat_completion_request(messages, model=GPT_MODEL):
     try:
@@ -26,135 +27,10 @@ def chat_completion_request(messages, model=GPT_MODEL):
         print(f"Exception: {e}")
         return e
 
+
 messages = []
 messages.append({"role": "system",
                  "content": "Тебя зовут Ala-Too GPT. Когда нужно, проводи профоиентационные тесты основываясь на потребностях пользователя. Приоритетнее проводить тест Климова. Проведи тест задавая вопросы по одному. Затем дай ему итоги теста и на основе результатов советуй им программы обучение в Международном Университете Ала-Тоо. Твои сообщения должны быть предельно точными и короткими."})
-
-################################################################################
-### Step 6
-################################################################################
-
-def remove_newlines(serie):
-    serie = serie.str.replace('\n', ' ')
-    serie = serie.str.replace('\\n', ' ')
-    serie = serie.str.replace('  ', ' ')
-    serie = serie.str.replace('  ', ' ')
-    return serie
-
-# Create a list to store the text files
-texts = []
-
-# Open the file and read the text
-with open("Ala-Too GPT.txt", "r", encoding="UTF-8") as f:
-    text = f.read()
-
-    # Replace -, _, and #update with spaces.
-    texts.append(("Ala-Too GPT", text))
-
-# Create a dataframe from the list of texts
-df = pd.DataFrame(texts, columns=['fname', 'text'])
-
-# Set the text column to be the raw text with the newlines removed
-df['text'] = df.fname + ". " + remove_newlines(df.text)
-df.to_csv('processed/scraped.csv')
-df.head()
-
-################################################################################
-### Step 7
-################################################################################
-
-# Load the cl100k_base tokenizer which is designed to work with the ada-002 model
-tokenizer = tiktoken.get_encoding("cl100k_base")
-
-df = pd.read_csv('processed/scraped.csv', index_col=0)
-df.columns = ['title', 'text']
-
-# Tokenize the text and save the number of tokens to a new column
-df['n_tokens'] = df.text.apply(lambda x: len(tokenizer.encode(x)))
-
-# Visualize the distribution of the number of tokens per row using a histogram
-df.n_tokens.hist()
-
-################################################################################
-### Step 8
-################################################################################
-
-max_tokens = 800
-
-# Function to split the text into chunks of a maximum number of tokens
-def split_into_many(text, max_tokens=max_tokens):
-    # Split the text into sentences
-    sentences = text.split('. ')
-
-    # Get the number of tokens for each sentence
-    n_tokens = [len(tokenizer.encode(" " + sentence)) for sentence in sentences]
-
-    chunks = []
-    tokens_so_far = 0
-    chunk = []
-
-    # Loop through the sentences and tokens joined together in a tuple
-    for sentence, token in zip(sentences, n_tokens):
-
-        # If the number of tokens so far plus the number of tokens in the current sentence is greater
-        # than the max number of tokens, then add the chunk to the list of chunks and reset
-        # the chunk and tokens so far
-        if tokens_so_far + token > max_tokens:
-            chunks.append(". ".join(chunk) + ".")
-            chunk = []
-            tokens_so_far = 0
-
-        # If the number of tokens in the current sentence is greater than the max number of
-        # tokens, go to the next sentence
-        if token > max_tokens:
-            continue
-
-        # Otherwise, add the sentence to the chunk and add the number of tokens to the total
-        chunk.append(sentence)
-        tokens_so_far += token + 1
-
-    # Add the last chunk to the list of chunks
-    if chunk:
-        chunks.append(". ".join(chunk) + ".")
-
-    return chunks
-
-shortened = []
-
-# Loop through the dataframe
-for row in df.iterrows():
-
-    # If the text is None, go to the next row
-    if row[1]['text'] is None:
-        continue
-
-    # If the number of tokens is greater than the max number of tokens, split the text into chunks
-    if row[1]['n_tokens'] > max_tokens:
-        shortened += split_into_many(row[1]['text'])
-
-    # Otherwise, add the text to the list of shortened texts
-    else:
-        shortened.append(row[1]['text'])
-
-################################################################################
-### Step 9
-################################################################################
-
-df = pd.DataFrame(shortened, columns=['text'])
-df['n_tokens'] = df.text.apply(lambda x: len(tokenizer.encode(x)))
-df.n_tokens.hist()
-
-################################################################################
-### Step 10
-################################################################################
-
-# Note that you may run into rate limit issues depending on how many files you try to embed
-# Please check out our rate limit guide to learn more on how to handle this: https://platform.openai.com/docs/guides/rate-limits
-
-df['embeddings'] = df.text.apply(
-    lambda x: client.embeddings.create(input=[x], model='text-embedding-3-small').data[0].embedding)
-df.to_csv('processed/embeddings.csv')
-df.head()
 
 ################################################################################
 ### Step 11
@@ -165,9 +41,6 @@ df['embeddings'] = df['embeddings'].apply(literal_eval).apply(np.array)
 
 df.head()
 
-################################################################################
-### Step 12
-################################################################################
 
 ################################################################################
 ### Step 12
@@ -223,6 +96,7 @@ def create_context(
     # Return the context
     return "\n\n###\n\n".join(returns)
 
+
 def answer_question(
         question,
         df,
@@ -243,19 +117,18 @@ def answer_question(
         max_len=max_len,
         size=size,
     )
-    # If debug, print the raw model response
+
+    messages_with_context = messages + [{"role": "system", "content": context}]
+    messages_with_context.append({"role": "user", "content": question})
+
     if debug:
         print("Context:\n" + context)
         print("\n\n")
 
     try:
-        # Create a completions using the question and context
         response = client.chat.completions.create(
             model=model,
-            messages=[
-                {"role": "system", "content": context},
-                {"role": "user", "content": question}
-            ],
+            messages=messages_with_context,
             max_tokens=max_tokens,
             temperature=0,
             top_p=1,
@@ -267,6 +140,7 @@ def answer_question(
     except Exception as e:
         print(e)
         return ""
+
 
 ################################################################################
 ### Step 13
