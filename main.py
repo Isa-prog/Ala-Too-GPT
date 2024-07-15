30b085959d371b2acc6a43b5a97937c08b0945d4
isakasymbekuulu <isakasymbekuulu@gmail.com>
2024-07-15 17:34:02 +0600

    ready version 1.0

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import os
 from ast import literal_eval
-
 import numpy as np
 import openai
 import pandas as pd
@@ -31,6 +30,132 @@ def chat_completion_request(messages, model=GPT_MODEL):
 messages = []
 messages.append({"role": "system",
                  "content": "Тебя зовут Ala-Too GPT. Когда нужно, проводи профориентационные тесты, основываясь на потребностях пользователя. Приоритетнее проводить тест Климова. Проведи тест, задавая вопросы по одному. Затем дай ему итоги теста и на основе результатов советуй им программы обучения в Международном Университете Ала-Тоо. Твои сообщения должны быть предельно точными и короткими."})
+
+# ################################################################################
+# ### Step 6
+# ################################################################################
+#
+# def remove_newlines(serie):
+#     serie = serie.str.replace('\n', ' ')
+#     serie = serie.str.replace('\\n', ' ')
+#     serie = serie.str.replace('  ', ' ')
+#     serie = serie.str.replace('  ', ' ')
+#     return serie
+#
+# # Create a list to store the text files
+# texts = []
+#
+# # Open the file and read the text
+# with open("Ala-Too GPT.txt", "r", encoding="UTF-8") as f:
+#     text = f.read()
+#
+#     # Replace -, _, and #update with spaces.
+#     texts.append(("Ala-Too GPT", text))
+#
+# # Create a dataframe from the list of texts
+# df = pd.DataFrame(texts, columns=['fname', 'text'])
+#
+# # Set the text column to be the raw text with the newlines removed
+# df['text'] = df.fname + ". " + remove_newlines(df.text)
+# df.to_csv('processed/scraped.csv')
+# df.head()
+#
+# ################################################################################
+# ### Step 7
+# ################################################################################
+#
+# # Load the cl100k_base tokenizer which is designed to work with the ada-002 model
+# tokenizer = tiktoken.get_encoding("cl100k_base")
+#
+# df = pd.read_csv('processed/scraped.csv', index_col=0)
+# df.columns = ['title', 'text']
+#
+# # Tokenize the text and save the number of tokens to a new column
+# df['n_tokens'] = df.text.apply(lambda x: len(tokenizer.encode(x)))
+#
+# # Visualize the distribution of the number of tokens per row using a histogram
+# df.n_tokens.hist()
+#
+# ################################################################################
+# ### Step 8
+# ################################################################################
+#
+# max_tokens = 800
+#
+# # Function to split the text into chunks of a maximum number of tokens
+# def split_into_many(text, max_tokens=max_tokens):
+#     # Split the text into sentences
+#     sentences = text.split('. ')
+#
+#     # Get the number of tokens for each sentence
+#     n_tokens = [len(tokenizer.encode(" " + sentence)) for sentence in sentences]
+#
+#     chunks = []
+#     tokens_so_far = 0
+#     chunk = []
+#
+#     # Loop through the sentences and tokens joined together in a tuple
+#     for sentence, token in zip(sentences, n_tokens):
+#
+#         # If the number of tokens so far plus the number of tokens in the current sentence is greater
+#         # than the max number of tokens, then add the chunk to the list of chunks and reset
+#         # the chunk and tokens so far
+#         if tokens_so_far + token > max_tokens:
+#             chunks.append(". ".join(chunk) + ".")
+#             chunk = []
+#             tokens_so_far = 0
+#
+#         # If the number of tokens in the current sentence is greater than the max number of
+#         # tokens, go to the next sentence
+#         if token > max_tokens:
+#             continue
+#
+#         # Otherwise, add the sentence to the chunk and add the number of tokens to the total
+#         chunk.append(sentence)
+#         tokens_so_far += token + 1
+#
+#     # Add the last chunk to the list of chunks
+#     if chunk:
+#         chunks.append(". ".join(chunk) + ".")
+#
+#     return chunks
+#
+# shortened = []
+#
+# # Loop through the dataframe
+# for row in df.iterrows():
+#
+#     # If the text is None, go to the next row
+#     if row[1]['text'] is None:
+#         continue
+#
+#     # If the number of tokens is greater than the max number of tokens, split the text into chunks
+#     if row[1]['n_tokens'] > max_tokens:
+#         shortened += split_into_many(row[1]['text'])
+#
+#     # Otherwise, add the text to the list of shortened texts
+#     else:
+#         shortened.append(row[1]['text'])
+#
+# ################################################################################
+# ### Step 9
+# ################################################################################
+#
+# df = pd.DataFrame(shortened, columns=['text'])
+# df['n_tokens'] = df.text.apply(lambda x: len(tokenizer.encode(x)))
+# df.n_tokens.hist()
+#
+# ################################################################################
+# ### Step 10
+# ################################################################################
+#
+# # Note that you may run into rate limit issues depending on how many files you try to embed
+# # Please check out our rate limit guide to learn more on how to handle this: https://platform.openai.com/docs/guides/rate-limits
+#
+# df['embeddings'] = df.text.apply(
+#     lambda x: client.embeddings.create(input=[x], model='text-embedding-3-small').data[0].embedding)
+# df.to_csv('processed/embeddings.csv')
+# df.head()
 
 ################################################################################
 ### Step 11
@@ -105,12 +230,12 @@ def answer_question(
         df,
         messages,
         model=GPT_MODEL,
-        max_len=1000,  # уменьшено максимальное количество токенов контекста
+        max_len=800,  # уменьшено максимальное количество токенов контекста
         size="small",
         debug=False,
         max_tokens=150,  # уменьшено максимальное количество токенов для ответа
-        temperature=0.5,  # изменение параметра temperature для генерации более точных ответов
-        top_p=0.9,  # изменение параметра top_p для управления разнообразием ответа
+        temperature=0.7,  # изменение параметра temperature для генерации более точных ответов
+        top_p=1,  # изменение параметра top_p для управления разнообразием ответа
         stop_sequence=None
 ):
     """
